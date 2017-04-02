bc1fbae07cc4aa1b30e09a74f5f1cf4fc079505f
Silverhawk <sondra.silverhawk@hush.com>
2017-04-02 16:02:45 -0700

    Search by word, style

--- client/js/bundle.js
+++ client/js/bundle.js
@@ -2,16 +2,54 @@
 var $ = require('jquery');
 var data = require('./netflix_genres.json');
 
-function searchGenres(q) {
-  if(data[q]) {
-    var url = "https://www.netflix.com/browse/genre/" + data[q];
-    var node = "<a href=" + url + " target='_blank'>" + q + "</a>";
-    $('#result').append(node);
+function appendLinkNode(genre) {
+  var url = "https://www.netflix.com/browse/genre/" + data[genre];
+  var node = "<a href=" + url + " target='_blank'>" + genre + "</a>";
+  $('#result').append(node);
+}
+
+function searchGenresByKey(keyWord) {
+  
+  var matches = [];
+  for(key in data) {
+    var words = key.match(/[a-zA-Z]+/g);
+    for(var i = 0; i < words.length; i++ ) {
+      var wordLower = words[i].toLowerCase();
+      var qLower = keyWord.toLowerCase();
+      var matched = true;
+      for(var j = 0; j < qLower.length; j++ ) {
+        if(qLower[j] !== wordLower[j]) {
+          matched = false;
+        }
+      }
+      if(matched) {
+        matches.push(key);
+        continue;
+      }
+    }
+  }
+  if(matches.length) {
+    for(var i = 0; i < matches.length; i++) {
+      appendLinkNode(matches[i]);
+    }
+  }
+}
+
+function searchGenres(query) {
+  // if the exact genre is queried just append
+  if(data[query]) {
+    return appendLinkNode(query);
+  }
+  // otherwise look for all word matches
+  var terms = query.match(/[a-zA-Z]+/g);
+  for(var i = 0; i < terms.length; i++) {
+    searchGenresByKey(terms[i]);
   }
 }
 
 $(function() {
   $('#search-form form').submit(function(event) {
+    $('#result').empty();
     var text = $('input#category').val();
     event.preventDefault();
     searchGenres(text);
@@ -23,7 +61,7 @@ $(function() {
 });
 
 },{"./netflix_genres.json":2,"jquery":3}],2:[function(require,module,exports){
-module.exports={"Action & Adventure":"136","Action Comedies":"4304","Action Sci-Fi & Fantasy":"156","Action Thrillers":"4304","Adult Animation":"1188","Adventures":"744","African Movies":"376","Alien Sci-Fi":"332","Animal Tales":"550","Anime":"742","Anime Action":"265","Anime Comedies":"930","Anime Dramas":"45","Anime Fantasy":"1114","Anime Features":"306","Anime Horror":"1069","Anime Sci-Fi":"272","Anime Series":"672","Art House Movies":"2976","Asian Action Movies":"7723","Australian Movies":"523","B-Horror Movies":"819","Baseball Movies":"1233","Basketball Movies":"1276","Belgian Movies":"26","Biographical Documentaries":"365","Biographical Dramas":"317","Boxing Movies":"1244","British Movies":"1075","British TV Shows":"5211","Campy Movies":"125","Children & Family Movies":"78","Chinese Movies":"396","Classic Action & Adventure":"4657","Classic Comedies":"3169","Classic Dramas":"2980","Classic Foreign Movies":"3247","Classic Movies":"3157","Classic Musicals":"3239","Classic Romantic Movies":"3127","Classic Sci-Fi & Fantasy":"4714","Classic Thrillers":"4658","Classic TV Shows":"4655","Classic War Movies":"4874","Classic Westerns":"4746","Comedies":"654","Comic Book and Superhero Movies":"1011","Country & Western/Folk":"110","Courtroom Dramas":"274","Creature Features":"689","Crime Action & Adventure":"958","Crime Documentaries":"987","Crime Dramas":"688","Crime Thrillers":"1049","Crime TV Shows":"2614","Cult Comedies":"943","Cult Horror Movies":"1094","Cult Movies":"762","Cult Sci-Fi & Fantasy":"473","Cult TV Shows":"7465","Dark Comedies":"86","Deep Sea Horror Movies":"4502","Disney":"6767","Disney Musicals":"5943","Documentaries":"683","Dramas":"576","Dramas based on Books":"496","Dramas based on real life":"365","Dutch Movies":"1060","Eastern European Movies":"525","Education for Kids":"1065","Epics":"5285","Experimental Movies":"1107","Faith & Spirituality":"2683","Faith & Spirituality Movies":"5280","Family Features":"5105","Fantasy Movies":"974","Film Noir":"768","Food & Travel TV":"7243","Football Movies":"1280","Foreign Action & Adventure":"1182","Foreign Comedies":"442","Foreign Documentaries":"516","Foreign Dramas":"215","Foreign Gay & Lesbian Movies":"824","Foreign Horror Movies":"865","Foreign Movies":"746","Foreign Sci-Fi & Fantasy":"648","Foreign Thrillers":"1030","French Movies":"5880","Gangster Movies":"3185","Gay & Lesbian Dramas":"50","German Movies":"5888","Greek Movies":"6111","Historical Documentaries":"534","Horror Comedy":"8958","Horror Movies":"871","Independent Action & Adventure":"1180","Independent Comedies":"419","Independent Dramas":"38","Independent Movies":"707","Independent Thrillers":"326","Indian Movies":"1046","Irish Movies":"5875","Italian Movies":"822","Japanese Movies":"1039","Jazz & Easy Listening":"1027","Kids Faith & Spirituality":"75142","Kids Music":"5284","Kids’ TV":"2734","Korean Movies":"568","Korean TV Shows":"6787","Late Night Comedies":"140","Latin American Movies":"161","Latin Music":"1074","Martial Arts Movies":"898","Martial Arts, Boxing & Wrestling":"669","Middle Eastern Movies":"587","Military Action & Adventure":"212","Military Documentaries":"400","Military Dramas":"1","Military TV Shows":"2580","Miniseries":"481","Mockumentaries":"2","Monster Movies":"94","Movies based on children’s books":"1005","Movies for ages 0 to 2":"679","Movies for ages 2 to 4":"621","Movies for ages 5 to 7":"545","Movies for ages 8 to 10":"56","Movies for ages 11 to 12":"696","Music & Concert Documentaries":"9036","Music":"170","Musicals":"1333","Mysteries":"999","New Zealand Movies":"6378","Period Pieces":"1212","Political Comedies":"270","Political Documentaries":"701","Political Dramas":"661","Political Thrillers":"1050","Psychological Thrillers":"550","Quirky Romance":"3610","Reality TV":"983","Religious Documentaries":"1000","Rock & Pop Concerts":"327","Romantic Comedies":"547","Romantic Dramas":"125","Romantic Favorites":"50267","Romantic Foreign Movies":"715","Romantic Independent Movies":"991","Romantic Movies":"888","Russian":"1156","Satanic Stories":"699","Satires":"492","Scandinavian Movies":"929","Sci-Fi & Fantasy":"149","Sci-Fi Adventure":"692","Sci-Fi Dramas":"391","Sci-Fi Horror Movies":"169","Sci-Fi Thrillers":"1101","Science & Nature Documentaries":"259","Science & Nature TV":"5278","Screwball Comedies":"970","Showbiz Dramas":"501","Showbiz Musicals":"1357","Silent Movies":"5331","Slapstick Comedies":"1025","Slasher and Serial Killer Movies":"864","Soccer Movies":"1254","Social & Cultural Documentaries":"367","Social Issue Dramas":"394","Southeast Asian Movies":"919","Spanish Movies":"5874","Spiritual Documentaries":"276","Sports & Fitness":"932","Sports Comedies":"528","Sports Documentaries":"18","Sports Dramas":"724","Sports Movies":"437","Spy Action & Adventure":"1070","Spy Thrillers":"914","Stage Musicals":"5577","Stand-up Comedy":"1155","Steamy Romantic Movies":"3580","Steamy Thrillers":"97","Supernatural Horror Movies":"4202","Supernatural Thrillers":"1114","Tearjerkers":"638","Teen Comedies":"351","Teen Dramas":"929","Teen Screams":"5214","Teen TV Shows":"6095","Thrillers":"893","Travel & Adventure Documentaries":"115","TV Action & Adventure":"1067","TV Cartoons":"1117","TV Comedies":"1037","TV Documentaries":"1010","TV Dramas":"1171","TV Horror":"8305","TV Mysteries":"436","TV Sci-Fi & Fantasy":"137","TV Shows":"8","Urban & Dance Concerts":"947","Vampire Horror Movies":"7580","Werewolf Horror Movies":"7593","Westerns":"770","World Music Concerts":"285","Zombie Horror Movies":"75405"}
+module.exports={"Action & Adventure":"1365","Action Comedies":"43040","Action Sci-Fi & Fantasy":"1568","Action Thrillers":"43048","Adult Animation":"11881","Adventures":"7442","African Movies":"3761","Alien Sci-Fi":"3327","Animal Tales":"5507","Anime":"7424","Anime Action":"2653","Anime Comedies":"9302","Anime Dramas":"452","Anime Fantasy":"11146","Anime Features":"3063","Anime Horror":"10695","Anime Sci-Fi":"2729","Anime Series":"6721","Art House Movies":"29764","Asian Action Movies":"77232","Australian Movies":"5230","B-Horror Movies":"8195","Baseball Movies":"12339","Basketball Movies":"12762","Belgian Movies":"262","Biographical Documentaries":"3652","Biographical Dramas":"3179","Boxing Movies":"12443","British Movies":"10757","British TV Shows":"52117","Campy Movies":"1252","Children & Family Movies":"783","Chinese Movies":"3960","Classic Action & Adventure":"46576","Classic Comedies":"31694","Classic Dramas":"29809","Classic Foreign Movies":"32473","Classic Movies":"31574","Classic Musicals":"32392","Classic Romantic Movies":"31273","Classic Sci-Fi & Fantasy":"47147","Classic Thrillers":"46588","Classic TV Shows":"46553","Classic War Movies":"48744","Classic Westerns":"47465","Comedies":"6548","Comic Book and Superhero Movies":"10118","Country & Western/Folk":"1105","Courtroom Dramas":"2748","Creature Features":"6895","Crime Action & Adventure":"9584","Crime Documentaries":"9875","Crime Dramas":"6889","Crime Thrillers":"10499","Crime TV Shows":"26146","Cult Comedies":"9434","Cult Horror Movies":"10944","Cult Movies":"7627","Cult Sci-Fi & Fantasy":"4734","Cult TV Shows":"74652","Dark Comedies":"869","Deep Sea Horror Movies":"45028","Disney":"67673","Disney Musicals":"59433","Documentaries":"6839","Dramas":"5763","Dramas based on Books":"4961","Dramas based on real life":"3653","Dutch Movies":"10606","Eastern European Movies":"5254","Education for Kids":"10659","Epics":"52858","Experimental Movies":"11079","Faith & Spirituality":"26835","Faith & Spirituality Movies":"52804","Family Features":"51056","Fantasy Movies":"9744","Film Noir":"7687","Food & Travel TV":"72436","Football Movies":"12803","Foreign Action & Adventure":"11828","Foreign Comedies":"4426","Foreign Documentaries":"5161","Foreign Dramas":"2150","Foreign Gay & Lesbian Movies":"8243","Foreign Horror Movies":"8654","Foreign Movies":"7462","Foreign Sci-Fi & Fantasy":"6485","Foreign Thrillers":"10306","French Movies":"58807","Gangster Movies":"31851","Gay & Lesbian Dramas":"500","German Movies":"58886","Greek Movies":"61115","Historical Documentaries":"5349","Horror Comedy":"89585","Horror Movies":"8711","Independent Action & Adventure":"11804","Independent Comedies":"4195","Independent Dramas":"384","Independent Movies":"7077","Independent Thrillers":"3269","Indian Movies":"10463","Irish Movies":"58750","Italian Movies":"8221","Japanese Movies":"10398","Jazz & Easy Listening":"10271","Kids Faith & Spirituality":"751423","Kids Music":"52843","Kids’ TV":"27346","Korean Movies":"5685","Korean TV Shows":"67879","Late Night Comedies":"1402","Latin American Movies":"1613","Latin Music":"10741","Martial Arts Movies":"8985","Martial Arts, Boxing & Wrestling":"6695","Middle Eastern Movies":"5875","Military Action & Adventure":"2125","Military Documentaries":"4006","Military Dramas":"11","Military TV Shows":"25804","Miniseries":"4814","Mockumentaries":"26","Monster Movies":"947","Movies based on children’s books":"10056","Movies for ages 0 to 2":"6796","Movies for ages 2 to 4":"6218","Movies for ages 5 to 7":"5455","Movies for ages 8 t":"10","Movies for age":"11","t":"12","Music & Concert Documentaries":"90361","Music":"1701","Musicals":"13335","Mysteries":"9994","New Zealand Movies":"63782","Period Pieces":"12123","Political Comedies":"2700","Political Documentaries":"7018","Political Dramas":"6616","Political Thrillers":"10504","Psychological Thrillers":"5505","Quirky Romance":"36103","Reality TV":"9833","Religious Documentaries":"10005","Rock & Pop Concerts":"3278","Romantic Comedies":"5475","Romantic Dramas":"1255","Romantic Favorites":"502675","Romantic Foreign Movies":"7153","Romantic Independent Movies":"9916","Romantic Movies":"8883","Russian":"11567","Satanic Stories":"6998","Satires":"4922","Scandinavian Movies":"9292","Sci-Fi & Fantasy":"1492","Sci-Fi Adventure":"6926","Sci-Fi Dramas":"3916","Sci-Fi Horror Movies":"1694","Sci-Fi Thrillers":"11014","Science & Nature Documentaries":"2595","Science & Nature TV":"52780","Screwball Comedies":"9702","Showbiz Dramas":"5012","Showbiz Musicals":"13573","Silent Movies":"53310","Slapstick Comedies":"10256","Slasher and Serial Killer Movies":"8646","Soccer Movies":"12549","Social & Cultural Documentaries":"3675","Social Issue Dramas":"3947","Southeast Asian Movies":"9196","Spanish Movies":"58741","Spiritual Documentaries":"2760","Sports & Fitness":"9327","Sports Comedies":"5286","Sports Documentaries":"180","Sports Dramas":"7243","Sports Movies":"4370","Spy Action & Adventure":"10702","Spy Thrillers":"9147","Stage Musicals":"55774","Stand-up Comedy":"11559","Steamy Romantic Movies":"35800","Steamy Thrillers":"972","Supernatural Horror Movies":"42023","Supernatural Thrillers":"11140","Tearjerkers":"6384","Teen Comedies":"3519","Teen Dramas":"9299","Teen Screams":"52147","Teen TV Shows":"60951","Thrillers":"8933","Travel & Adventure Documentaries":"1159","TV Action & Adventure":"10673","TV Cartoons":"11177","TV Comedies":"10375","TV Documentaries":"10105","TV Dramas":"11714","TV Horror":"83059","TV Mysteries":"4366","TV Sci-Fi & Fantasy":"1372","TV Shows":"83","Urban & Dance Concerts":"9472","Vampire Horror Movies":"75804","Werewolf Horror Movies":"75930","Westerns":"7700","World Music Concerts":"2856","Zombie Horror Movies":"75405"}
 },{}],3:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.2.1
--- main.js
+++ main.js
@@ -1,16 +1,54 @@
 var $ = require('jquery');
 var data = require('./netflix_genres.json');
 
-function searchGenres(q) {
-  if(data[q]) {
-    var url = "https://www.netflix.com/browse/genre/" + data[q];
-    var node = "<a href=" + url + " target='_blank'>" + q + "</a>";
-    $('#result').append(node);
+function appendLinkNode(genre) {
+  var url = "https://www.netflix.com/browse/genre/" + data[genre];
+  var node = "<a href=" + url + " target='_blank'>" + genre + "</a>";
+  $('#result').append(node);
+}
+
+function searchGenresByKey(keyWord) {
+  
+  var matches = [];
+  for(key in data) {
+    var words = key.match(/[a-zA-Z]+/g);
+    for(var i = 0; i < words.length; i++ ) {
+      var wordLower = words[i].toLowerCase();
+      var qLower = keyWord.toLowerCase();
+      var matched = true;
+      for(var j = 0; j < qLower.length; j++ ) {
+        if(qLower[j] !== wordLower[j]) {
+          matched = false;
+        }
+      }
+      if(matched) {
+        matches.push(key);
+        continue;
+      }
+    }
+  }
+  if(matches.length) {
+    for(var i = 0; i < matches.length; i++) {
+      appendLinkNode(matches[i]);
+    }
+  }
+}
+
+function searchGenres(query) {
+  // if the exact genre is queried just append
+  if(data[query]) {
+    return appendLinkNode(query);
+  }
+  // otherwise look for all word matches
+  var terms = query.match(/[a-zA-Z]+/g);
+  for(var i = 0; i < terms.length; i++) {
+    searchGenresByKey(terms[i]);
   }
 }
 
 $(function() {
   $('#search-form form').submit(function(event) {
+    $('#result').empty();
     var text = $('input#category').val();
     event.preventDefault();
     searchGenres(text);
